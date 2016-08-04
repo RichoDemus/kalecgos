ec1ef31f3e1131d151bad8f197305de3cb56e1ee
RichoDemus <richodemus@gmail.com>
2016-08-04 23:47:27 +0200

    Now prints the contents of all toc-files

--- src/github.com/richodemus/wow/HelloWorld.go
+++ src/github.com/richodemus/wow/HelloWorld.go
@@ -5,14 +5,34 @@ package main
 import (
 	"fmt"
 	"flag"
+	"io/ioutil"
+	"strings"
+	"log"
 )
 
 func main() {
 	addonsDirectory := flag.String("addons-directory", "c:\\Games\\World of Warcraft\\Interface\\AddOns\\", "Path to the Addons folder")
 
 	flag.Parse()
+	strPointerValue := *addonsDirectory
 
-	//argsWithoutProg := os.Args[1:]
 	fmt.Println("Hello World!")
-	fmt.Println("Dir:", *addonsDirectory)
+	fmt.Println("Dir:", strPointerValue)
+
+	addonDirectories, _ := ioutil.ReadDir(strPointerValue)
+	for _, addonDirectory := range addonDirectories {
+		fmt.Println(strPointerValue +  addonDirectory.Name())
+		filesInAddonDirectory, _ := ioutil.ReadDir(strPointerValue +  addonDirectory.Name() + "/")
+		for _, filesInAddonDirectory := range filesInAddonDirectory {
+			if strings.HasSuffix(filesInAddonDirectory.Name(), ".toc") {
+				tocFile, err := ioutil.ReadFile(strPointerValue +  addonDirectory.Name() + "/" + filesInAddonDirectory.Name())
+				if err != nil {
+				    log.Fatal(err)
+				}
+				fmt.Println(string(tocFile))
+
+			}
+
+		}
+	}
 }
