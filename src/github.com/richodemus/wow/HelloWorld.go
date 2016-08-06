c6f295f7ac58e51fd924b457651d0ed4defdd03e
RichoDemus <richodemus@gmail.com>
2016-08-06 17:55:12 +0200

    Cleaned up the code a bit

--- src/github.com/richodemus/wow/HelloWorld.go
+++ src/github.com/richodemus/wow/HelloWorld.go
@@ -11,25 +11,28 @@ import (
 )
 
 func main() {
-	addonsDirectory := flag.String("addons-directory", "c:\\Games\\World of Warcraft\\Interface\\AddOns\\", "Path to the Addons folder")
+	addonsDirectoryPointer := flag.String("addons-directory", "c:\\Games\\World of Warcraft\\Interface\\AddOns\\", "Path to the Addons folder")
 
 	flag.Parse()
-	strPointerValue := *addonsDirectory
+	addonsDirectory := *addonsDirectoryPointer
 
 	fmt.Println("Hello World!")
-	fmt.Println("Dir:", strPointerValue)
+	fmt.Println("Dir:", addonsDirectory)
 
-	addonDirectories, _ := ioutil.ReadDir(strPointerValue)
+	addonDirectories, _ := ioutil.ReadDir(addonsDirectory)
 	for _, addonDirectory := range addonDirectories {
-		fmt.Println(strPointerValue +  addonDirectory.Name())
-		filesInAddonDirectory, _ := ioutil.ReadDir(strPointerValue +  addonDirectory.Name() + "/")
-		for _, filesInAddonDirectory := range filesInAddonDirectory {
-			if strings.HasSuffix(filesInAddonDirectory.Name(), ".toc") {
-				tocFile, err := ioutil.ReadFile(strPointerValue +  addonDirectory.Name() + "/" + filesInAddonDirectory.Name())
+		fmt.Println("Addon: " + addonsDirectory +  addonDirectory.Name())
+		filesInAddonDirectory, err := ioutil.ReadDir(addonsDirectory + "/" + addonDirectory.Name() + "/")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, addonFile := range filesInAddonDirectory {
+			if strings.HasSuffix(addonFile.Name(), ".toc") {
+				tocFile, err := ioutil.ReadFile(addonsDirectory + "/" + addonDirectory.Name() + "/" + addonFile.Name())
 				if err != nil {
 				    log.Fatal(err)
 				}
-				fmt.Println(string(tocFile))
+				fmt.Println("Tocfile: " + string(tocFile))
 
 			}
 
